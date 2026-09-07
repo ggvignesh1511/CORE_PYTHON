e81d9f6622ac0f28445faa72fdb96e6570e1a1ce
GOURI VIGNESH GEMBALI <ggvignesh15@gmail.com>
2026-09-07 14:50:08 +0530

    removed self.count

--- ITERATORS/PROGRAM_3.py
+++ ITERATORS/PROGRAM_3.py
@@ -1,14 +1,12 @@
 class EvenNumbers:
     def __init__(self, n):
-        self.count = 0
         self.n = n
         self.num = 0
     def __iter__(self):
         return self
     def __next__(self):
-        if self.count < self.n:
+        if self.num < self.n:
             self.num += 2
-            self.count += 1
             return self.num
         raise StopIteration
 
